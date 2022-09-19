56463560a0b6c47bdbad11c01f053810e80c5e18
Sergey Mishnev <s.mishnev@forsit.de>
2022-09-19 08:45:21 +0300

    feat: added component: - input - search - textArea added page search

--- components/index.ts
+++ components/index.ts
@@ -7,3 +7,6 @@ export * from './Card/Card';
 export * from './Sort/Sort';
 export * from './HhData/HhData';
 export * from './Advantages/Advantages';
+export * from './Input/Input';
+export * from './Textarea/Textarea';
+export * from './Search/Search';
